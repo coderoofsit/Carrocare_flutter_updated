a64fa2be0b9b89e93fa34fbeb3f44281c94666cf
Nikunj Shukla <coderoofitsolutions@gmail.com>
2026-08-26 10:51:45 +0530

    version updated

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Android version - 1.7.16+51
-deployed on - 17 Aug 2026
+Android version - 1.7.17+52
+deployed on - 25 Aug 2026
 
-Ios version - 1.6.12+54
-deployed ob - 10 Augg 2026
+Ios version - 1.6.12+58
+deployed ob - 25 Augg 2026
 
 Branch name = final-version
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 51
-        versionName = "1.0.16"
+        versionCode = 52
+        versionName = "1.0.17"
     }
 
     buildTypes {
